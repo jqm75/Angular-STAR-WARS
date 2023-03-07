e085f96f7e9e1ed30c1d527fd1a6ec2618590b7f
Quim <joaquimpujol@gmail.com>
2023-03-07 18:49:54 +0100

    Project Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+import { SharedComponent } from './shared/shared.component';
+import { AuthComponent } from './content/auth/auth.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StarshipsComponent,
+    LoginComponent,
+    ContentComponent,
+    SharedComponent,
+    AuthComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
