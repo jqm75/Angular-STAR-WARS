a39cf9217770b79fad3753b2d6cf008e41f6a8b5
Quim <joaquimpujol@gmail.com>
2023-03-07 20:05:32 +0100

    Fix app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { StarshipsComponent } from './starships/starships.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './content/home/home.component';
 import { ContentComponent } from './content/content.component';
-import { SharedComponent } from './shared/shared.component';
 import { AuthComponent } from './content/auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { StarshipsComponent } from './content/starships/starships.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     StarshipsComponent,
-    LoginComponent,
     ContentComponent,
     SharedComponent,
     AuthComponent,
